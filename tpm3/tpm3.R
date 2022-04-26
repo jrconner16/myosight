976d11d534b0a2dfaad5aee86ae4114e6fcd111c
Jamie Conner <jrconner16@gmail.com>
2022-04-26 16:27:04 -0400

    added min feret plot to R code

--- tpm3/tpm3.R
+++ tpm3/tpm3.R
@@ -5,18 +5,15 @@
 ####
 #### packages
 ####
-library(tidyr)
-library(ggplot2)
+library(tidyverse)
 library(vroom)
-library(dplyr)
-library(stringr)
+library(plotrix)
+library(RColorBrewer)
+library(grid)
+library(gridExtra)
 
-
-install.packages("stringr")
-
-
-
-
+my_pallete<- brewer.pal(4, "GnBu")
+brewer.pal(4, "GnBu")
 
 list_of_files <- list.files(path = ".", recursive = TRUE,
                             pattern = "\\.txt$", 
@@ -24,35 +21,55 @@ list_of_files <- list.files(path = ".", recursive = TRUE,
 
 
 
-df <- vroom(list_of_files, id="Results")   #creats DF with folder path as column
+df <- vroom(list_of_files, id="file")   #creats DF with folder path as column
 
-
+#################3creates a genotype or in this blinded case a color column#####
 df_black<- df %>% 
-    mutate(Color= str_extract(df$Results, "BLACK")) %>%   ####cleans file path column
+    mutate(Color= str_extract(df$file, "BLACK")) %>%   ####cleans file path column
     filter(Color=="BLACK") %>% 
-    select("Color", "Label", "Area", "MinFeret")
-           
+    select("file", "Color", "Label", "Area", "MinFeret")
 df_red<- df %>% 
-  mutate(Color= str_extract(df$Results, "RED")) %>% 
+  mutate(Color= str_extract(df$file, "RED")) %>% 
   filter(Color=="RED") %>% 
-  select("Color", "Label", "Area", "MinFeret")
-
+  select("file","Color", "Label", "Area", "MinFeret")
 df<- rbind(df_black, df_red)
 
-df$Color<- factor(df$Color)
-df$Label<- factor(df$Label)
-df_red$Color<- factor(df_red$Color)
-df_red$Label<- factor(df_red$Label)
-df_black$Color<- factor(df_black$Color)
-df_black$Label<- factor(df_black$Label)
+df$id <- cumsum(!duplicated(df[1])) ######creates ID column based on unique file names
+View(df)
+###############################################################
+##########################PLOT#################################
+###############################################################
 
-BLACK<- summary(df_red)
-RED<- summary(df_black)
+######factor for plots###############
+df<-df %>%
+  mutate_at(vars(id, Color), factor)
+
+df_grouped<- group_by(df, id, Color)
+
+minFeret_theme<- function(){
+}
+  
+####minFeret 
+      ######stats
+      df_stat<- df_grouped %>% 
+                mutate(mean=mean(MinFeret), se=std.error(MinFeret), sd=sd(MinFeret)) 
+
+    
+
+p_minFer_byLabel<-ggplot(df_stat, aes(x=df_stat$Color, y=df_stat$MinFeret, fill=Label)) +
+          geom_boxplot()+
+          labs(title= "minimum Feret diameter", x="genotypes", y='minFere(uM')+
+          scale_fill_discrete(name = "Fiber Type")
+p_minFer_byLabel
 
 
-class(BLACK)
+p_minFer_byColor<-ggplot(df_stat, aes(x=df_stat$Color, y=df_stat$MinFeret, fill=Color)) +
+  scale_fill_manual(values= c("grey", "red"))+
+  labs(title= "minimum Feret diameter", x="genotypes", y='minFere(uM')+
+  geom_boxplot()+
+ 
+  minFeret_theme()
+p_minFer_byColor
 
-summaryByColor<- rbind(BLACK,RED)
+grid.arrange(p_minFer_byColor, p_minFer_byLabel)
 
-write.csv(summaryByColor, "summaryByColor.csv")
-write.csv(RED, "summaryRed.csv")
